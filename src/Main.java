2568139ac3bb029bcb5120d7f0d08e958b093079
Витя <balezino2017@gmail.com>
2025-03-30 21:06:56 +0300

    программа с выводом на экран

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args) {
-
+        ImageIcon icon = new ImageIcon("E:\\Китай\1.jpg");
+        JOptionPane.showMessageDialog(
+                null,
+                "Настройки сохранены",
+                "Готово",
+                JOptionPane.PLAIN_MESSAGE,
+                icon
+        );
     }
 }
