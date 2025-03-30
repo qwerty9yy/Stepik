f9e8a9b8c3e1b212052eeeee180051d1641ce017
Витя <balezino2017@gmail.com>
2025-03-30 16:59:06 +0300

    Добавил в Main класс самолет

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import Transport.Air.*;
+import Transport.Transport;
 
 public class Main {
     public static void main(String[] args) {
-
+        Airplane airplane = new Airplane();
+        System.out.println(airplane);
+        Transport.People.displayPeopleTransport();
     }
 }
