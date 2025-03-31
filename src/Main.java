86f64b835bcaba0765507a80e319bab1e518dc77
Витя <balezino2017@gmail.com>
2025-03-31 20:16:15 +0300

    Потоки ввода-вывода. Работа с файлами

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,2 @@
-import java.io.*;
-
-public class Main {
-    public static void main(String[] args) {
-        String text = "Привет мир!"; // строка для записи
-        try(FileOutputStream fos = new FileOutputStream("H:\\java\\GitHabSTEPIK\\primer.txt");
-            PrintStream printStream = new PrintStream(fos))
-        {
-            printStream.println(text);
-            System.out.println("Запись в файл произведена");
-        }
-        catch(IOException ex){
-            System.out.println(ex.getMessage());
-        }
-    }
-}
+https://metanit.com/java/tutorial/6.1.php
+для изучения потоки ввода-вывода
