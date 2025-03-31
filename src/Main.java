b347164dbcee52768df238c1f73c3a5b77845bc1
Витя <balezino2017@gmail.com>
2025-03-31 21:03:05 +0300

    код для добавления в файл текст, если такого файла нет то создает его

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
-https://metanit.com/java/tutorial/6.1.php
-для изучения потоки ввода-вывода
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args){
+        String text = "Hello world!";
+        try(FileOutputStream fos = new FileOutputStream("new.txt")) {
+            //перевод строки в байты
+            byte[] buffer = text.getBytes();
+            fos.write(buffer, 0, buffer.length);
+            System.out.println("The file has been written");
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+}
