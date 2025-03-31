95ae527805a02292b3bfa901172c1d21d075b3f0
Витя <balezino2017@gmail.com>
2025-03-31 21:34:46 +0300

    считали файл

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,22 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args){
-        String text = "Hello world!";
-        try(FileOutputStream fos = new FileOutputStream("new.txt")) {
-            //перевод строки в байты
-            byte[] buffer = text.getBytes();
-            fos.write(buffer, 0, buffer.length);
-            System.out.println("The file has been written");
+        try(FileInputStream fin = new FileInputStream("new.txt"))
+        {
+            byte[] bufer = new byte[256];
+            System.out.println("File data:");
+
+            int count;
+            while((count = fin.read(bufer)) != -1)
+            {
+                for(int i = 0; i < count; i++)
+                {
+                    System.out.print((char)bufer[i]);
+                }
+            }
         }
-        catch(IOException ex){
+        catch (IOException ex)
+        {
             System.out.println(ex.getMessage());
         }
     }
