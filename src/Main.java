35cc841143dc54ceae01298d6c1258cdaeac87e8
Витя <balezino2017@gmail.com>
2025-03-31 20:13:59 +0300

    код для записи в файл текст

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
-import javax.swing.ImageIcon;
-import javax.swing.JOptionPane;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        ImageIcon icon = new ImageIcon("E:\\Китай\1.jpg");
-        JOptionPane.showMessageDialog(
-                null,
-                "Настройки сохранены",
-                "Готово",
-                JOptionPane.PLAIN_MESSAGE,
-                icon
-        );
+        String text = "Привет мир!"; // строка для записи
+        try(FileOutputStream fos = new FileOutputStream("H:\\java\\GitHabSTEPIK\\primer.txt");
+            PrintStream printStream = new PrintStream(fos))
+        {
+            printStream.println(text);
+            System.out.println("Запись в файл произведена");
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
     }
 }
