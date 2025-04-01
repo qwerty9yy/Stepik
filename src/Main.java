c74343b99c75ac321e6ec8dad2d3d348d9549fae
Витя <balezino2017@gmail.com>
2025-04-01 13:14:29 +0300

    Закрытие потоков сокращеный вариант автоматически вызывает close() при выходе из блока

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 import java.io.*;
 
-public class Main {
+public class Main{
     public static void main(String[] args){
-        FileInputStream fin = null;
-        try {
-            fin = new FileInputStream("C:\\Users\\Lite\\Desktop\\java\\файлы\\test.txt");
+        try(FileInputStream fin = new FileInputStream("C:\\Users\\Lite\\Desktop\\java\\файлы\\test.txt")) {
             int i = -1;
             while((i = fin.read()) != -1){
                 System.out.print((char)i);
@@ -12,15 +10,6 @@ public class Main {
         }
         catch(IOException ex) {
             System.out.println(ex.getMessage());
-        }
-        finally {
-            try {
-                if(fin != null)
-                    fin.close();
-            }
-            catch(IOException ex) {
-                System.out.println(ex.getMessage());
-            }
         }
     }
 }
