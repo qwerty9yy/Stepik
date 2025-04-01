37060bd5d7404fe759b0e71470b1b2fb442eab9f
Витя <balezino2017@gmail.com>
2025-04-01 14:48:51 +0300

    Создание файла или перезаписывание файла с добавлением текста используя ByteArrayOutputStream

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,21 @@
 import java.io.*;
 
-public class Main {
+public class Main{
     public static void main(String[] args) {
-        byte[] array1 = new byte[]{1, 3, 5, 7};
-        ByteArrayInputStream byteStream1 = new ByteArrayInputStream(array1);
-        int b;
-        while((b = byteStream1.read()) != -1) {
-            System.out.print(b);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        String text = "Hello Wolrd!";
+        byte[] buffer = text.getBytes();
+        try {
+            baos.write(buffer);
         }
-        String text = "Hello word!";
-        byte[] array2 = text.getBytes();
-        //Считываем 5 символов
-        ByteArrayInputStream byteStream2 = new ByteArrayInputStream(array2, 0, 5);
-        int c;
-        while((c = byteStream2.read()) != -1) {
-            System.out.print((char)c);
+        catch(Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        try(FileOutputStream fos = new FileOutputStream("hello.txt")) {
+            baos.writeTo(fos);
+        }
+        catch(IOException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
