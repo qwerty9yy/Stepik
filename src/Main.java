c9334abeb2c300e08dd6432a9fb87378a896e2bd
Витя <balezino2017@gmail.com>
2025-04-09 14:30:28 +0300

    class File. Работа с файлами и каталогами

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,30 @@
-class Person implements Serializable{
+import java.io.*;
 
-    private String name;
-    private int age;
-    private transient double height;
-    private transient boolean married;
-
-    Person(String n, int a, double h, boolean m){
-
-        name=n;
-        age=a;
-        height=h;
-        married=m;
+public class Main {
+    public static void main(String[] args){
+        // определяем объект для каталога
+        File myFile = new File("C://SomeDir//readme.txt");
+        System.out.println("File name: " + myFile.getName());
+        System.out.println("Parent folder: " + myFile.getParent());
+        if(myFile.exists())
+            System.out.println("Файл существует");
+        else
+            System.out.println("Файл не существует");
+        System.out.println("File size: " + myFile.length());
+        if(myFile.canRead())
+            System.out.println("Файл может быть прочитан");
+        else System.out.println("Файл нельзя прочитать");
+        if(myFile.canWrite())
+            System.out.println("Файл может быть записан");
+        else System.out.println("Файл не может быть записан");
+        // создадим новый файл
+        File newFile = new File("C://SomeDir//new1");
+        try{
+            boolean new1 = newFile.createNewFile();
+            if(new1)
+                System.out.println("Файл создан");
+        } catch(IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
-    String getName() {return name;}
-    int getAge(){ return age;}
-    double getHeight(){return height;}
-    boolean getMarried(){return married;}
 }
