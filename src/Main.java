a94be60c2c759ac8e9e51577bba8a87afec9eb1c
Витя <balezino2017@gmail.com>
2025-04-10 07:02:32 +0300

    Пример в Java чтения и записи в текстовый файл

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,21 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args){
-        // получаем консоль
-        Console console = System.console();
-        if(console != null){
-            // считываем данные с консоли
-            String login = console.readLine("Введите логин:");
-            char[] passwd = console.readPassword("Введите пароль:");
-
-            console.printf("Введенный логин: %s \n", login);
-            console.printf("Введенный пароль: %s \n", new String(passwd));
-        }
+       File file = new File("My.txt");
+       try(FileWriter writer = new FileWriter(file))
+       {
+           String text = "Это простой пример,\nв котором мы осуществляем\nс помощью языка Java\nзапись в файл\nи чтение из файла\n";
+           writer.write(text);
+       }catch(IOException ex) {
+           System.out.println(ex.getMessage());
+       }
+       try(FileReader fr = new FileReader(file))
+       {
+           char[] buffer = new char[(int)file.length()];
+           fr.read(buffer);
+           System.out.print(new String(buffer));
+       }catch(IOException ex){
+           System.out.println(ex.getMessage());
+       }
     }
 }
