cf1d5eaf131da51fbac05ec31954abdc36b64801
Витя <balezino2017@gmail.com>
2025-04-11 13:14:27 +0300

    Калькулятор

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        String text = sc.nextLine(); //Создали перемнную для ввода выражения
-        String[] str = text.split(" ");//Разделяем на пробелы
-
-        //Проверка для длину вводимых данных
-        if(str.length != 3){
-            System.out.println("The length does not match");
-            return;
+        String text = sc.nextLine();
+        String[] num = text.split(" ");
+        double a, b;
+        try{
+            a = Double.parseDouble(num[0]);
+            b = Double.parseDouble(num[2]);
+            String ops = num[1];
+            if (!ops.equals("+") && !ops.equals("-") && !ops.equals("*") && !ops.equals("/")){
+                System.out.println("Operation Error!" );
+            }
+            switch (ops) {
+                case "+":
+                    System.out.println(a + b);
+                    break;
+                case "-":
+                    System.out.println(a - b);
+                    break;
+                case "*":
+                    System.out.println(a * b);
+                    break;
+                case "/":
+                    System.out.println(b == 0 ? "Error! Division by zero" : a / b);
+                    break;
+            }
+        } catch (Exception ex){
+            System.out.println("Error! Not number");
         }
-
     }
 }
