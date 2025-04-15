ec46141416a0b060bb04208b108fe04a14d5ac76
Витя <balezino2017@gmail.com>
2025-04-15 12:14:13 +0300

    Пробное окно с двумя кнопками

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,27 @@
-import java.io.*;
-import java.util.Scanner;
+import javax.swing.*;
 
-public class Main {
+public class Main extends JFrame{
+    private static final long serialVersionUID = 1L;
+
+    public Main(){
+        super("Test ContentPane");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        JPanel contents = new JPanel();
+        contents.add(new JButton("Школа"));
+
+        JButton button = new JButton("Семья");
+        button.addActionListener(e -> System.out.println("Кнопка нажата!"));
+        contents.add(button);
+
+        setContentPane(contents);
+
+        setSize(200,100);
+
+        setVisible(true);
+    }
     public static void main(String[] args){
-       try(BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
-           FileWriter fileWriter = new FileWriter("output.txt")){
-
-           String line;
-           while((line = reader.readLine()) != null){
-               line = line.trim();
-               if(line.isEmpty()) continue;
-               String[] arr = line.split("\\s+");
-
-               if(arr.length != 3)
-                   fileWriter.write(line + " = Ошибка! Не правильное кол-во\n");
-
-               try {
-                   double a = Double.parseDouble(arr[0]);
-                   double b = Double.parseDouble(arr[2]);
-
-                   String sign = arr[1];
-                   if(!sign.equals("+") && !sign.equals("-") && !sign.equals("/") && !sign.equals("*")){
-                       fileWriter.write(line + " = Знак не соответствует заданию\n");
-                   }
-
-                   double result;
-                   switch(sign){
-                       case "+": result = a + b; break;
-                       case "-": result = a - b; break;
-                       case "/":
-                           if(b == 0){
-                               fileWriter.write(line + " = на ноль делить нельзя\n");
-                               continue;
-                           }
-                           result = a / b; break;
-                       case "*": result = a * b; break;
-                       default: continue;
-                   }
-                   fileWriter.write(line + " = " + result + "\n");
-               }catch(NumberFormatException ex){
-                   fileWriter.write(line + " = Не является числом\n");
-               }
-           }
-       }catch(IOException ex){
-           System.out.println("Ошибка: " + ex.getMessage());
-
-       }
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        new Main();
     }
 }
