c4a4e95c479d80d1d578ceea6ebbab9f6185f693
Витя <balezino2017@gmail.com>
2025-04-15 13:14:01 +0300

    Graphics.clipRect

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,21 @@ import java.awt.Graphics;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public  class Main extends JPanel{
-    public void paintComponent(Graphics g){
-        super.paintComponent(g);
-        g.setColor(Color.RED);
-        g.clearRect(0, 0, 100, 100);
+public class Main extends JPanel{
+    public void paint(Graphics g){
+        // 1. Рисуем черный прямоугольник
+        g.setColor(Color.black);
+        g.drawRect(0, 0, 100, 100);
+        // 2. Устанавливаем область отсечения
+        g.clipRect(25, 25, 50, 50);
+        // 3. Рисуем линию (будет видна только в области отсечения)
+        g.drawLine(0, 100, 100, 0);
     }
-    public static void main(String[] args){
+    public static void  main(String[] args){
         JFrame frame = new JFrame();
         frame.add(new Main());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setBounds(20,20, 500,500);
+        frame.setBounds(20, 20, 500, 500);
         frame.setVisible(true);
     }
 }
